9a3137891645d3a17a39e384f917ff03a96d1e48
Isnawan25 <isnawanrifai@gmail.com>
2024-03-19 10:34:18 +0700

    TUGASKE02

--- app/src/main/java/com/example/greeting/MainActivity.kt
+++ app/src/main/java/com/example/greeting/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.text.style.LineHeightSpan
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,12 +15,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.AlignmentLine
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.greeting.ui.theme.GreetingTheme
+import org.w3c.dom.Text
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,7 +34,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ){
-                    GreetingText(pesan = "Happy Graduation Anne!!" , pengirim ="Carl Jhonson" )
+                    GreetingText(pesan = "Untuk Alexander Paijo", isi = "Harapan Adalah mimpi yang Nyata",
+                        pengirim ="Isnawan Rifai Saputra" )
 
                 }
             }
@@ -40,32 +44,55 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GreetingText(pesan: String, pengirim: String, modifier: Modifier= Modifier) {
+fun GreetingText(pesan: String, isi: String, pengirim: String, modifier: Modifier= Modifier) {
     Column (
-        verticalArrangement = Arrangement.Center,
-        modifier = modifier.padding(8.dp)
+        verticalArrangement = Arrangement.Top,
+        modifier = modifier.fillMaxSize().background(Color.Black)
 
-            ){
+    ) {
         Text(
             text = pesan,
-            fontSize = 75.sp,
-            textAlign = TextAlign.Center,
-            LineHeight = 110.sp
-
-        )
-        Text(
-            text = pengirim,
-            fontSize = 28.sp,
-            modifier = Modifier.padding(16.dp).align(alignment = Alignment.End)
+            color = Color.Green,
+            fontSize = 55.sp,
+            lineHeight = 45.sp,
+            textAlign = TextAlign.Center
         )
     }
+        Column (
+                verticalArrangement = Arrangement.Center)
+        {
+        Text(
+            text =isi,
+            color = Color.LightGray,
+            fontSize = 40.sp,
+            lineHeight = 40.sp,
+            textAlign = TextAlign.Center
+        )}
+    Column (
+        verticalArrangement = Arrangement.Bottom
+    ){
+
+
+        Text(
+            text = pengirim,
+            color = Color.Green,
+            fontSize = 38.sp,
+            lineHeight = 35.sp,
+            modifier = Modifier.padding(5.dp).align
+                (alignment = Alignment.End)
+
+        )
+
+    }
+
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     GreetingTheme {
-        GreetingText(pesan = "Happy Graduation Anne!!",
-        pengirim = "Carl Jhonson")
+        GreetingText(pesan = "Untuk Alexander Paijo",isi = "Harapan adalah mimpi yag Nyata",
+        pengirim = "Isnawan Rifai Saputra")
     }
 }
