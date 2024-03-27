9f6bc97128f51b068b83224257457b47f82c12c8
Isnawan25 <isnawanrifai@gmail.com>
2024-03-27 13:57:33 +0700

    Composable Image

--- app/src/main/java/com/example/greeting/MainActivity.kt
+++ app/src/main/java/com/example/greeting/MainActivity.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.text.style.LineHeightSpan
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -17,6 +19,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -34,8 +40,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ){
-                    GreetingText(pesan = "Untuk Alexander Paijo", isi = "Harapan Adalah mimpi yang Nyata",
-                        pengirim ="Isnawan Rifai Saputra" )
+                    imageback(pesan = "Untuk Alexander Paijo", isi = "Harapan Adalah mimpi yang Nyata",
+                        pengirim ="Isnawan Rifai Saputra")
 
                 }
             }
@@ -45,54 +51,83 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GreetingText(pesan: String, isi: String, pengirim: String, modifier: Modifier= Modifier) {
+val fontduwur = FontFamily(Font(R.font.diskonregular))
+    val fonttengah = FontFamily(Font(R.font.pemanka))
     Column (
         verticalArrangement = Arrangement.Top,
-        modifier = modifier.fillMaxSize().background(Color.Black)
 
     ) {
         Text(
             text = pesan,
-            color = Color.Green,
-            fontSize = 55.sp,
+            color = Color.Black,
+            fontSize = 50.sp,
             lineHeight = 45.sp,
-            textAlign = TextAlign.Center
+            textAlign = TextAlign.Center,
+            fontFamily = fontduwur
         )
     }
         Column (
-                verticalArrangement = Arrangement.Center)
+                verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxSize())
         {
         Text(
             text =isi,
-            color = Color.LightGray,
-            fontSize = 40.sp,
+            color = Color.White,
+            fontSize = 50.sp,
             lineHeight = 40.sp,
-            textAlign = TextAlign.Center
+            textAlign = TextAlign.Center,
+            fontFamily = fonttengah,
+            modifier = Modifier.padding(50.dp)
         )}
     Column (
-        verticalArrangement = Arrangement.Bottom
+        verticalArrangement = Arrangement.Bottom,
+        horizontalAlignment = Alignment.End,
+        modifier = Modifier.fillMaxSize()
     ){
 
 
         Text(
             text = pengirim,
-            color = Color.Green,
+            color = Color.Black,
             fontSize = 38.sp,
-            lineHeight = 35.sp,
-            modifier = Modifier.padding(5.dp).align
-                (alignment = Alignment.End)
+            lineHeight =35.sp,
+            modifier = Modifier.padding(20.dp),
+            fontFamily = fontduwur
+
 
         )
 
     }
 
 }
+@Composable
+fun imageback(pesan: String, isi: String, pengirim: String, modifier: Modifier = Modifier){
+    val Gambar = painterResource(R.drawable.gambarback)
+    Box {
+        Image(
+            painter = Gambar,
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(),
+            alpha = 0.5F,
+            contentScale = ContentScale.Crop
+
+        )
+        GreetingText(
+            pesan = pesan,
+            isi = isi,
+            pengirim = pengirim,
+            modifier = Modifier.fillMaxSize())
+    }
+}
+
 
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     GreetingTheme {
-        GreetingText(pesan = "Untuk Alexander Paijo",isi = "Harapan adalah mimpi yag Nyata",
+        imageback(pesan = "Untuk Alexander Paijo",isi = "Harapan adalah mimpi yang Nyata",
         pengirim = "Isnawan Rifai Saputra")
     }
 }
